9256b8e97228ca3c6b0c8e34f7260dd56cdfb4a8
WescAlves <wesley.c.alves98@gmail.com>
2023-04-13 20:34:35 -0300

    Lista de exercícios 01

---
+++ Exercício 3/index.js
@@ -0,0 +1,28 @@
+let inputgasolina = document.querySelector("#gasolina");
+let inputalcool = document.querySelector("#alcool");
+let botao = document.querySelector("#botao");
+const calcularMelhorCombustivel = function(gasolina, alcool){
+    let result = alcool / gasolina;
+    if(result<0.7){
+        return 1;
+    }
+    else{ return 0} 
+}
+botao.addEventListener("click", function(){
+    let gasolina = inputgasolina.value;
+    let alcool = inputalcool.value;
+    let result = calcularMelhorCombustivel(gasolina, alcool);
+    
+    if(result==1){
+    let div = document.querySelector("div");
+    let paragrafo = document.createElement("p");
+    div.appendChild(paragrafo);
+    paragrafo.textContent = "O alcool é a melhor opção.";
+    }
+    else{
+        let div = document.querySelector("div");
+        let paragrafo = document.createElement("p");
+        div.appendChild(paragrafo);
+        paragrafo.textContent = "A gasolina é a melhor opção.";
+    }})
+    
